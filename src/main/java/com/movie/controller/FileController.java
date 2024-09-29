749d90ff559f754e0be7a00e7c27a16978a387d8
naveenrhindi <naveenhindi4@gmail.com>
2024-09-29 11:48:25 -0900

    File Uploading Done

--- src/main/java/com/movie/controller/FileController.java
+++ src/main/java/com/movie/controller/FileController.java
@@ -1,10 +1,12 @@
 package com.movie.controller;
 
 import com.movie.service.FileService;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
 
 @RestController
 @RequestMapping("/file/")
@@ -16,6 +18,13 @@ public class FileController {
         this.fileService = fileService;
     }
 
-//    public ResponseEntity<String> uploadFileHandler(@Re)
+    @Value("${projects.poster}")
+    private String path;
+
+    @PostMapping("/upload")
+    public ResponseEntity<String> uploadFileHandler(@RequestPart MultipartFile file) throws IOException {
+        String uploadedFileName = fileService.uploadFile(path, file);
+        return ResponseEntity.ok("File uploaded : "+uploadedFileName);
+    }
 
 }
